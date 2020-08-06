563dd371c780b6a1499c7263a6f571bbca9abecb
Matt Harris <mattharris@Matts-MBP.home>
2020-08-06 11:06:14 -0400

    register resources w/admin

--- shopping_cart/admin.py
+++ shopping_cart/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from shopping_cart.models import ShoppingCart, ShoppingCartProduct
 
 # Register your models here.
+admin.site.register(ShoppingCart)
+admin.site.register(ShoppingCartProduct)
--- tags/admin.py
+++ tags/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from tags.models import Tag
 
 # Register your models here.
+admin.site.register(Tag)
